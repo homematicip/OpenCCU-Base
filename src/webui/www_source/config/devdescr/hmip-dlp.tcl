26d8a294dcd575bf10d342c8cde87b96949a3dd4
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2025-08-25 10:08:58 +0200

    SPHM-1352 - HmIP-DLP(-A) - Preview picture added

---
+++ src/webui/www_source/config/devdescr/hmip-dlp.tcl
@@ -0,0 +1,19 @@
+#!/bin/tclsh
+
+#Typ dieser Beschreibung (Schlüssel)
+#-----------------------------------------------------------------------
+set TYPE "HmIP-DLP"
+
+#Beschreibung des Gerätetyps
+#-----------------------------------------------------------------------
+set DESCRIPTION "HmIP-DLP"
+
+#Pfade zu den Bildern verschiedener Größe
+#lappend PATHLIST <Pixellänge maximale Ausdehnung> <Pfad zum Bild>
+#-----------------------------------------------------------------------
+set     PATHLIST ""
+lappend PATHLIST [list  50	"/config/img/devices/50/248_hmip-dlp_thumb.png"]
+lappend PATHLIST [list  250	"/config/img/devices/250/248_hmip-dlp.png"]
+
+set P ""
+
