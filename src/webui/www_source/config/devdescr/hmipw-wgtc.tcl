4e3fc0d344b0a3749439d87a4899b9e43a4e7cce
gral-eq3 <211460013+gral-eq3@users.noreply.github.com>
2026-04-07 09:56:03 +0200

    SPHM-1456 - HmIPW-WGTC(-A) - WebUI description and preview picture added

---
+++ src/webui/www_source/config/devdescr/hmipw-wgtc.tcl
@@ -0,0 +1,18 @@
+#!/bin/tclsh
+
+#Typ dieser Beschreibung (Schlüssel)
+#-----------------------------------------------------------------------
+set TYPE "HmIPW-WGTC"
+
+#Beschreibung des Gerätetyps
+#-----------------------------------------------------------------------
+set DESCRIPTION "HmIPW-WGTC"
+
+#Pfade zu den Bildern verschiedener Größe
+#lappend PATHLIST <Pixellänge maximale Ausdehnung> <Pfad zum Bild>
+#-----------------------------------------------------------------------
+set     PATHLIST ""
+lappend PATHLIST [list  50	"/config/img/devices/50/240_hmip-wgtc-f_thumb.png"]
+lappend PATHLIST [list  250	"/config/img/devices/250/240_hmip-wgtc-f.png"]
+
+set P ""
